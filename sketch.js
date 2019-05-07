e36a01f4a131cfeefb6bd1ac63a1bab8b83b9d9b
Ryo Suzuki <aa371716@ee371716noMacBook-ea.local>
2019-05-07 23:04:10 +0900

    23:46

--- sketch.js
+++ sketch.js
@@ -57,7 +57,7 @@ function draw() {
 function shakeColors(){
   for (var i = 0; i < tileCountY; i++){
     colorsLeft[i] = color(random(0, 60), random(0, 100), 100);
-    colorsRight[i] = color(random(160, 190), random(0, 100), 100);
+    colorsRight[i] = color(random(0, 60), random(0, 100), 100);
   }
 }
 
