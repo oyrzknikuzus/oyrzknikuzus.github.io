e4c576ffdf4208a1adc8907d5b11a1b7bedd3542
Ryo Suzuki <aa371716@ee371716noMacBook-ea.local>
2019-05-12 21:05:10 +0900

    5/12 -5

--- sketch_main.js
+++ sketch_main.js
@@ -13,6 +13,7 @@ var sketch1 = function(p) {
     p.noCursor();
     p.clear();
     p.strokeCap(actStrokeCap);
+    //p.randomSeed(actRandomSeed);
 
     for (var gridY = 0; gridY <= tileCount; gridY++) {
       for (var gridX = 0; gridX <= tileCount; gridX++) {
@@ -39,16 +40,17 @@ var sketch1 = function(p) {
 
 var sketch2 = function(p) {
   p.setup = function() {
-    canvas = p.createCanvas(500, 500);
-    canvas.position(p.windowWidth * 0.7, 0);
+    canvas = p.createCanvas(p.windowWidth / 4, p.windowWidth / 4);
+    canvas.position(p.windowWidth * 0.75, -30);
     canvas.style('z-index', '-1');
+    //p.background(0);
   };
 
   p.draw = function() {
     p.frameRate(20);
     x = p.width / 2;
-    y = 150;
-    r = 180;
+    y = p.height / 2;
+    r = p.height * 0.5;
     p.fill(255, 10);
     p.noStroke();
     x = x + p.random(-5, 5);
