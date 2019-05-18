74731cd1e0b5ab50c89b232c39a1bbd11573f427
Ryo Suzuki <aa371716@ee371716noMacBook-ea.local>
2019-05-18 22:10:29 +0900

    5/18 -8

--- jquery.js
+++ jquery.js
@@ -1,10 +1,11 @@
+
 $(function(){
   $(window).scroll(function (){
     $(".fade_off").each(function(){
       var imgPos = $(this).offset().top;
       var scroll = $(window).scrollTop();
       var windowHeight = $(window).height();
-      if (scroll > imgPos - windowHeight){
+      if (scroll > imgPos - windowHeight + 900){
         $(this).addClass("scrollin");
       } else {
         $(this).removeClass("fade_on");
