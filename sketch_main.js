a79f5bb9688429d1620aa51a2d25462da4c75b36
Ryo Suzuki <aa371716@ee371716noMacBook-ea.local>
2019-05-19 15:01:07 +0900

    5/19 -9

--- sketch_main.js
+++ sketch_main.js
@@ -102,7 +102,7 @@ var sketch3 = function(p) {
     p.stroke(0);
     p.strokeWeight(0.5);
     for (i = 0; i < 10; i++){
-      p.text(sourceText.substring(i, i + 1), 150 + (20 * i) + p.random(-5, 5), 120);
+      p.text(sourceText.substring(i, i + 1), 130 + (20 * i) + p.random(-5, 5), 120);
       p.fill(p.random(255), p.random(255), 255);
     }
   };
