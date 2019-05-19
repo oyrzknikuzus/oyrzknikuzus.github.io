357d327b47832bacb107ee5c47d43cded07d5b77
Ryo Suzuki <aa371716@ee371716noMacBook-ea.local>
2019-05-19 15:34:20 +0900

    5/19 -11

--- jquery.js
+++ jquery.js
@@ -30,3 +30,18 @@ $(function(){
     });
   });
 });
+
+$(function(){
+  $(window).scroll(function (){
+    $(".fade_off3").each(function(){
+      var imgPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + 2600){
+        $(this).addClass("scrollin");
+      } else {
+        $(this).removeClass("fade_on");
+      }
+    });
+  });
+});
