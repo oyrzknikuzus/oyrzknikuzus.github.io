43f9b295398f2517e2424a42b48e5f93d8f045ad
Ryo Suzuki <aa371716@ee371716noMacBook-ea.local>
2019-06-16 20:20:49 +0900

    6/16 -17

--- jquery.js
+++ jquery.js
@@ -1,47 +1,96 @@
-
-$(function(){
-  $(window).scroll(function (){
-    $(".fade_off1").each(function(){
-      var imgPos = $(this).offset().top;
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll > imgPos - windowHeight + 100){
-        $(this).addClass("scrollin");
-      } else {
-        $(this).removeClass("fade_on");
-      }
+if (window.matchMedia( "(max-width: 768px)" ).matches) {
+  $(function(){
+    $(window).scroll(function (){
+      $(".fade_off1").each(function(){
+        var imgPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > imgPos - windowHeight + 100){
+          $(this).addClass("scrollin");
+        } else {
+          $(this).removeClass("fade_on");
+        }
+      });
     });
   });
-});
 
 
 
-$(function(){
-  $(window).scroll(function (){
-    $(".fade_off2").each(function(){
-      var imgPos = $(this).offset().top;
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll > imgPos - windowHeight + 2600){
-        $(this).addClass("scrollin");
-      } else {
-        $(this).removeClass("fade_on");
-      }
+  $(function(){
+    $(window).scroll(function (){
+      $(".fade_off2").each(function(){
+        var imgPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > imgPos - windowHeight + 2600){
+          $(this).addClass("scrollin");
+        } else {
+          $(this).removeClass("fade_on");
+        }
+      });
     });
   });
-});
 
-$(function(){
-  $(window).scroll(function (){
-    $(".fade_off3").each(function(){
-      var imgPos = $(this).offset().top;
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll > imgPos - windowHeight + 1300){
-        $(this).addClass("scrollin");
-      } else {
-        $(this).removeClass("fade_on");
-      }
+  $(function(){
+    $(window).scroll(function (){
+      $(".fade_off3").each(function(){
+        var imgPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > imgPos - windowHeight + 1300){
+          $(this).addClass("scrollin");
+        } else {
+          $(this).removeClass("fade_on");
+        }
+      });
     });
   });
-});
+}else{
+  $(function(){
+    $(window).scroll(function (){
+      $(".fade_off1").each(function(){
+        var imgPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > imgPos - windowHeight + 100){
+          $(this).addClass("scrollin");
+        } else {
+          $(this).removeClass("fade_on");
+        }
+      });
+    });
+  });
+
+
+
+  $(function(){
+    $(window).scroll(function (){
+      $(".fade_off2").each(function(){
+        var imgPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > imgPos - windowHeight + 2600){
+          $(this).addClass("scrollin");
+        } else {
+          $(this).removeClass("fade_on");
+        }
+      });
+    });
+  });
+
+  $(function(){
+    $(window).scroll(function (){
+      $(".fade_off3").each(function(){
+        var imgPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > imgPos - windowHeight + 1300){
+          $(this).addClass("scrollin");
+        } else {
+          $(this).removeClass("fade_on");
+        }
+      });
+    });
+  });
+
+}
