df1f79b7d9a68238d424dc729554188671823dcf
Ryo Suzuki <aa371716@ee371716noMacBook-ea.local>
2019-06-23 13:03:45 +0900

    6/23 -24

--- sketch_main.js
+++ sketch_main.js
@@ -243,7 +243,7 @@ if (windowWidth <= windowSm) {
 
         p.fill(255, 10);
         p.noStroke();
-        p.strokeWeight(3);
+        p.strokeWeight(1);
 
         x = x + p.random(-15, 15);
         y = y + p.random(-15, 15);
