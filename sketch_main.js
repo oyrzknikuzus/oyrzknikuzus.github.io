49ea90070cdc82ff312af34060f15310a470a584
Ryo Suzuki <aa371716@ee371716noMacBook-ea.local>
2019-06-23 13:26:56 +0900

    6/23 -36

--- sketch_main.js
+++ sketch_main.js
@@ -177,7 +177,7 @@ if (windowWidth <= windowSm) {
 
       p.setup = function() {
         canvas = p.createCanvas(p.windowWidth, p.windowHeight);
-        canvas.position(0, 13);
+        canvas.position(0, 0);
         //canvas.style('z-index', '-1');
         //p.background('#222222');
 
